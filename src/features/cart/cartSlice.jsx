967e1659c4e338ca6e365af8ef3afbd1bc4af090
BijetaSah <bijetasah@30gmail.com>
2025-11-15 10:18:30 +0530

    feat(cart): setup cartSlice add selectors for total price and quantity in cartSlice

---
+++ src/features/cart/cartSlice.jsx
@@ -0,0 +1,52 @@
+const { createSlice } = "@reduxjs/toolkit";
+
+const initialState = {
+  cart: [],
+};
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addItem(state, action) {
+      // paylaod= newitem
+      state.cart.push(action.payload);
+    },
+    deleteItem(state, action) {
+      // payload = id
+      state.cart = state.cart.filter((item) => item !== action.payload);
+    },
+    increaseItemQuantity(state, action) {
+      // payload = id
+      const item = state.cart.find((item) => item.id === action.payload);
+      item.quantity++;
+      item.totalPrice = item.quantity * item.price;
+    },
+    decreaseItemQuantity(state, action) {
+      // payload = id
+      const item = state.cart.find((item) => item.id === action.payload);
+      item.quantity++;
+      item.totalPrice = item.quantity * item.price;
+    },
+    clearCart(state) {
+      state.cart = [];
+    },
+  },
+});
+
+export const {
+  addItem,
+  deleteItem,
+  increaseItemQuantity,
+  decreaseItemQuantity,
+  clearCart,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
+
+export const getTotalQuantity = (state) =>
+  state.cart.cart.reduce((sum, curr) => sum + curr.quantity, 0);
+
+export const getTotalPrice = (state) =>
+  state.cart.cart.reduce((sum, curr) => sum + curr.totalPrice, 0);
+
+export const getCart = (state) => state.cart.cart;
